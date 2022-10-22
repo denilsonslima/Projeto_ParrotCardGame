d96582335ed77e2dcc87316f0e83e8750580677e
denilsonslima <denilsondslima@gmail.com>
2022-10-21 22:28:41 -0400

    CARTAS VIRADAS AO CLICAR

--- scripts.js
+++ scripts.js
@@ -1,12 +1,32 @@
+let count = 1;
+let cartas = "";
+const cartasUsuario = [];
+const img = ["./img/0.png", "./img/1.gif", 
+"./img/2.gif", "./img/3.gif", 
+"./img/4.gif", "./img/5.gif",
+"./img/6.gif", "./img/7.gif"]
+
 let qtdCartas = prompt("ESCOLHA O NÚMERO DE CARTAS:\nMínimo: 4        Máximo: 14        OBS . : Valores Pares");
 
 while (qtdCartas < 4 || qtdCartas > 14 || qtdCartas % 2 !== 0){
     qtdCartas = prompt("INSIRA UM VALOR VÁLIDO:\nMínimo: 4        Máximo: 14        OBS . : Valores Pares");
 }
 
-for(let i = qtdCartas/2; i > 1; i--){
-    let cartas = document.querySelector(".main .coluna-superior");
-    cartas.innerHTML += `<div class="cartas"><img src="./img/papagaio.png"></div>`
-    let cartas1 = document.querySelector(".main .coluna-inferior");
-    cartas1.innerHTML += `<div class="cartas"><img src="./img/papagaio.png"></div>`
+for(let i = 0; i < qtdCartas; i+=2){
+    cartas = document.querySelector(".main");
+    cartas.innerHTML += `
+    <ul class="card" onclick="clickCarta(this)">
+        <li class="back-face face"><img src="/img/0.png"></li>
+        <li class="front-face face"><img src="${img[count]}"></li>
+    </ul>`
+    cartas.innerHTML += `
+    <ul class="card" onclick="clickCarta(this)">
+        <li class="back-face face"><img src="/img/0.png"></li>
+        <li class="front-face face"><img src="${img[count]}"></li>
+    </ul>`
+    count++
+}
+
+function clickCarta (seletor){
+    seletor.classList.toggle("virada")
 }
